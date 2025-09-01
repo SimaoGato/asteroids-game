d09e0f0f2774487a956e6bed29874251e72ffaed
Simão Gato <simao.valedegato@gmail.com>
2025-09-01 11:55:42 +0100

    create player sprite and add it to screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from constants import * 
+from constants import *
+from player import *
 
 # this allows us to use code from
 # the open-source pygame library
@@ -7,19 +8,25 @@ import pygame
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
 
     print("Starting Asteroids!\n"
     f"Screen width: {SCREEN_WIDTH}\n"
     f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        t = clock.tick(60)
+        dt = t / 1000
 
 
 if __name__ == "__main__":
