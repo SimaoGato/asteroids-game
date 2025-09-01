ea798b3d9a6c1c51a8f8b7196b3f67083475fc48
Simão Gato <simao.valedegato@gmail.com>
2025-09-01 14:37:35 +0100

    add asteroids sprite to the game

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 # this allows us to use code from
 # the open-source pygame library
@@ -19,10 +21,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     _ = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while(True):
         for event in pygame.event.get():
